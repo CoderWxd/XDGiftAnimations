651cb423452d32b52190e30bba03143b227a0c2d
CoderWxd <871791148@qq.com>
2018-01-01 09:13:07 +0800

    '生命周期'

--- Gift_Animations/Class/AnimationsViewController.m
+++ Gift_Animations/Class/AnimationsViewController.m
@@ -41,6 +41,9 @@
 - (void)viewWillAppear:(BOOL)animated{
     [super viewWillAppear:animated];
     [self.navigationController setNavigationBarHidden:YES animated:animated];
+    
+    
+    
 }
 - (void)viewWillDisappear:(BOOL)animated{
     [super viewWillDisappear:animated];
